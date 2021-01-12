99bdcb7375ddf82be7026792f5c5ac853f828dcd
Luciana Trueba <luciana@avalith.net>
2021-01-12 11:32:59 -0300

    db and handlers added

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"log"
+	"github.com/lutrueba/microblog/handlers"
+	"github.com/lutrueba/microblog/bd"
+)
 func main(){
-	
+	if bd.ChequeoConnection() == 0 {
+		log.Fatal("Sin conexión a la BD")
+		return
+	}
+	handlers.Manejadores()
+
 }
