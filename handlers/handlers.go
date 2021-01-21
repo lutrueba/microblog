a213fbff8a85c0b76f35c6b83aa21267d2ebf79c
Luciana Trueba <luciana@avalith.net>
2021-01-21 11:31:05 -0300

    VerPerfil de Usuario

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -4,25 +4,26 @@ import (
 	"log"
 	"net/http"
 	"os"
-	
+
 	"github.com/gorilla/mux"
 	"github.com/lutrueba/microblog/middlew"
 	"github.com/lutrueba/microblog/routers"
 	"github.com/rs/cors"
 )
 
-/*Manejadores: seteo mi puerto, el handler y pongo a escuchar al servidor*/
-func Manejadores(){
+/*Manejadores - seteo mi puerto, el handler y pongo a escuchar al servidor*/
+func Manejadores() {
 	router := mux.NewRouter()
 
 	/*Por cada EndPoint vamos a tener un renglón de código que permita manejar la función correspondiente*/
 	router.HandleFunc("/registro", middlew.ChequeoBD(routers.Registro)).Methods("POST")
 	router.HandleFunc("/login", middlew.ChequeoBD(routers.Login)).Methods("POST")
+	router.HandleFunc("/verperfil", middlew.ChequeoBD(middlew.ValidoJWT(routers.VerPerfil))).Methods("POST")
 
 	PORT := os.Getenv("PORT")
-	if PORT == ""{
+	if PORT == "" {
 		PORT = "8080"
 	}
 	handler := cors.AllowAll().Handler(router)
-	log.Fatal(http.ListenAndServe(":"+PORT,handler))
+	log.Fatal(http.ListenAndServe(":"+PORT, handler))
 }
