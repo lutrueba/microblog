32f9ef0dbda7a6fad82214a238154d5e0a81fb2e
Luciana Trueba <luciana@avalith.net>
2021-01-21 11:43:58 -0300

    VerPerfil de Usuario

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -18,7 +18,7 @@ func Manejadores() {
 	/*Por cada EndPoint vamos a tener un renglón de código que permita manejar la función correspondiente*/
 	router.HandleFunc("/registro", middlew.ChequeoBD(routers.Registro)).Methods("POST")
 	router.HandleFunc("/login", middlew.ChequeoBD(routers.Login)).Methods("POST")
-	router.HandleFunc("/verperfil", middlew.ValidoJWT(routers.VerPerfil)).Methods("GET")
+	router.HandleFunc("/verperfil", middlew.ChequeoBD(middlew.ValidoJWT(routers.VerPerfil))).Methods("GET")
 
 	PORT := os.Getenv("PORT")
 	if PORT == "" {
